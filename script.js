4fdfaf9733cd43b6d6d79f274c7343b22f4183fc
Eric Grimes <erictgrimes@gmail.com>
2025-04-22 16:13:05 -0700

    finished nav and added first js from block 12

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 console.log("Hello World");
+
+//responsive design for nav
+
+const hamburg = document.querySelector(".hamburg");
+const linksContainer = document.querySelector(".nav_items");
+const links = document.querySelectorAll(".nav_menu_item");
+
+hamburg.addEventListener("click", () => {
+  linksContainer.classList.toggle("active");
+  hamburg.classList.toggle("active");
+});
+
+window.addEventListener("resize", () => {
+  if (window.matchMedia("(max-width: 550px)").matches) {
+    clsoeMenu();
+  }
+});
+
+if (window.matchMedia("(max-width: 550px)").matches) {
+  clsoeMenu();
+}
+
+function clsoeMenu() {
+  links.forEach((link) => {
+    links.addEventListener("click", () => {
+      linksContainer.classList.remove("active");
+      hamburg.classList.remove("active");
+    });
+  });
+}
+
+//end of nav respovie code
+
+//used for successful submission
+
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
